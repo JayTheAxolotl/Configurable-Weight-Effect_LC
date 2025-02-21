00c2e3f694c42aa869de0110c815dc31e59b5e1c
JayTheAxolotl <jacobmine2010@gmail.com>
2025-02-21 17:40:01 -0600

    Got the patches actually working

--- ConfigurableWeightEffect/Plugin.cs
+++ ConfigurableWeightEffect/Plugin.cs
@@ -1,22 +1,41 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using ConfigurableWeightEffect.Patches;
 using HarmonyLib;
+using MonoMod.ModInterop;
 
 namespace ConfigurableWeightEffect;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+[BepInPlugin(modGUID, modName, modVersion)]
+public class ConfigurableWeightEffect : BaseUnityPlugin
 {
+    public const string modGUID = "org.configurable-weight-effect.axian";
+    public const string modName = "Configurable Weight Effect";
+    public const string modVersion = "1.0.0";
+    
     internal static new ManualLogSource Logger;
+    
+    private readonly Harmony harmony = new Harmony(modGUID);
 
+    private static ConfigurableWeightEffect Instance;
+    
     private void Awake()
     {
         // Plugin startup logic
+        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        
         Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Logger.LogInfo($"Plugin {modGUID} is loaded!");
+        
+        harmony.PatchAll(typeof(ConfigurableWeightEffect));
+        harmony.PatchAll(typeof(PlayerControllerB_patches));
     }
 
-
+    
 
     // All debug code beyond this point, remove for final release
     public static string Weight;
